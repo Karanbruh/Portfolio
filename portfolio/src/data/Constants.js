09d5906b9fe5b1cd2168c2d7acc44c9b1d384c96
Dhruv Panchal <dhruv@example.com>
2025-07-23 01:28:41 +0530

    last

--- portfolio/src/data/Constants.js
+++ portfolio/src/data/Constants.js
@@ -4,8 +4,8 @@ export const Bio = {
     name: "Karan Panchal",
     roles: [
         "Full Stack Developer",
-        "Mern Stack Developer",
         "Website Developer",
+        "Mern Stack Developer",
     ],
     description:
         "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
